709b73e11b1ca033b5a8e33891d0f3b83cbd8f7a
Daniel Peiró <danielpeiro72@gmail.com>
2023-10-19 18:38:39 +0200

    Remove unnecesary code

--- src/Ookly.Web/Pages/Privacy.cshtml.cs
+++ src/Ookly.Web/Pages/Privacy.cshtml.cs
@@ -1,20 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿namespace Ookly.Web.Pages;
 
-namespace Ookly.Web.Pages
+public class PrivacyModel : PageModel
 {
-    public class PrivacyModel : PageModel
+    public void OnGet()
     {
-        private readonly ILogger<PrivacyModel> _logger;
-
-        public PrivacyModel(ILogger<PrivacyModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-        }
     }
-
 }
