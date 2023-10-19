2a2f41913391e3502bf0075d331458beeaee3d3a
Daniel Peiró <danielpeiro72@gmail.com>
2023-10-20 00:34:34 +0200

    Add home page

--- src/Ookly.Web/Pages/Index.cshtml.cs
+++ src/Ookly.Web/Pages/Index.cshtml.cs
@@ -2,7 +2,16 @@ namespace Ookly.Web.Pages;
 
 public class IndexModel : PageModel
 {
-    public void OnGet()
+    public IReadOnlyCollection<Country> Countries { get; private set; } = [];
+
+    public async Task OnGetAsync()
     {
+        Countries = [new(1, "USA", [new(1, "Category 1"), new(2, "Category 2")])];
+
+        await Task.CompletedTask;
     }
 }
+
+public record Country(int Id, string Name, IReadOnlyCollection<Category> Categories);
+
+public record Category(int Id, string Name);
