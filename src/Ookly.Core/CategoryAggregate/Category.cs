fc8f47e1c3a4b054e67b93c941af2a7f32c11bd4
Daniel Peiró <danielpeiro72@gmail.com>
2023-10-20 20:33:10 +0200

    Entity is only int type

--- src/Ookly.Core/CategoryAggregate/Category.cs
+++ src/Ookly.Core/CategoryAggregate/Category.cs
@@ -4,8 +4,9 @@ using Ookly.Core.AdAggregate;
 
 namespace Ookly.Core.CategoryAggregate;
 
-public class Category(string name) : Entity<int>, IAggregateRoot
+public class Category(string name) : Entity, IAggregateRoot
 {
     public string Name { get; private set; } = name;
+
     public IReadOnlyList<Ad> Ads { get; private set; } = default!;
 }
