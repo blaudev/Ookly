cab73e80039653f0301494a8cdf0e03183ad25c6
blaudev <danielpeiro72@gmail.com>
2023-11-05 20:41:06 +0100

    Uncomment

--- src/Ookly.Core/Entities/Category.cs
+++ src/Ookly.Core/Entities/Category.cs
@@ -2,7 +2,7 @@
 
 public class Category : NamedEntity
 {
-    // public List<Filter> Filters { get; init; } = [];
-    // public List<CountryCategory> CountryCategories { get; init; } = [];
+    public List<Filter> Filters { get; init; } = [];
+    public List<CountryCategory> CountryCategories { get; init; } = [];
     //public List<ListingDetailType> ListingDetailTypes { get; init; } = [];
 }
